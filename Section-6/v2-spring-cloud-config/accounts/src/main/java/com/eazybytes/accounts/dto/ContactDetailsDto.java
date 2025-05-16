27c7960e131c72301c52e7ab61b73b6ff1c21e72
Manish Kummar <manishk57107@gmail.com>
2025-05-16 09:53:53 +0530

    section 4

--- Section-6/v2-spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/dto/ContactDetailsDto.java
+++ Section-6/v2-spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/dto/ContactDetailsDto.java
@@ -1,5 +1,7 @@
 package com.eazybytes.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
@@ -7,11 +9,35 @@ import java.util.Map;
 
 
 @ConfigurationProperties(prefix = "accounts")
-public record ContactDetailsDto(
-        String message,
-        Map<String, String> contactDetails,
-        List<String> onCallSupport
-) {
+@Getter
+@Setter
+public class ContactDetailsDto{
 
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, String> getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(Map<String, String> contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+
+    public List<String> getOnCallSupport() {
+        return onCallSupport;
+    }
+
+    public void setOnCallSupport(List<String> onCallSupport) {
+        this.onCallSupport = onCallSupport;
+    }
 }
