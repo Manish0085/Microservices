c3e4ef09fd69863e58aa772bf1b42009dacc8d80
Manish0085 <manishk57107@gmail.com>
2025-08-31 21:34:04 +0530

    Section 9 craeting gatewayserver for eazy bank microservices

---
+++ Section-9/accounts/src/main/java/com/eazybytes/accounts/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,59 @@
+package com.eazybytes.accounts.service.impl;
+
+import com.eazybytes.accounts.constants.CustomerController;
+import com.eazybytes.accounts.dto.AccountsDto;
+import com.eazybytes.accounts.dto.CardDto;
+import com.eazybytes.accounts.dto.CustomerDetailsDto;
+import com.eazybytes.accounts.dto.LoansDto;
+import com.eazybytes.accounts.entity.Accounts;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.exception.ResourceNotFoundException;
+import com.eazybytes.accounts.mapper.AccountsMapper;
+import com.eazybytes.accounts.mapper.CustomerMapper;
+import com.eazybytes.accounts.repo.AccountsRepository;
+import com.eazybytes.accounts.repo.CustomerRepository;
+import com.eazybytes.accounts.service.ICustomerService;
+import com.eazybytes.accounts.service.client.CardsFeignClient;
+import com.eazybytes.accounts.service.client.LoansFeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceImpl implements ICustomerService {
+
+
+    private CustomerRepository customerRepository;
+    private AccountsRepository accountsRepository;
+    private CardsFeignClient cardsFeignClient;
+    private LoansFeignClient loansFeignClient;
+
+
+    public CustomerServiceImpl(CustomerRepository customerRepository, AccountsRepository accountsRepository, CardsFeignClient cardsFeignClient, LoansFeignClient loansFeignClient) {
+        this.customerRepository = customerRepository;
+        this.accountsRepository = accountsRepository;
+        this.cardsFeignClient = cardsFeignClient;
+        this.loansFeignClient = loansFeignClient;
+    }
+
+
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+        );
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString())
+        );
+
+        CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
+        customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
+
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(mobileNumber);
+        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+
+        ResponseEntity<CardDto> cardDtoResponseEntity = cardsFeignClient.fetchCardDetails(mobileNumber);
+        customerDetailsDto.setCardsDto(cardDtoResponseEntity.getBody());
+
+        return customerDetailsDto;
+    }
+}
---
+++ Section-9/loans/src/main/java/com/eazybytes/loans/dto/LoansContactDetailsDto.java
@@ -0,0 +1,41 @@
+package com.eazybytes.loans.dto;
+
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+import java.util.Map;
+
+
+@ConfigurationProperties(prefix = "loans")
+public class LoansContactDetailsDto {
+
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, String> getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(Map<String, String> contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+
+    public List<String> getOnCallSupport() {
+        return onCallSupport;
+    }
+
+    public void setOnCallSupport(List<String> onCallSupport) {
+        this.onCallSupport = onCallSupport;
+    }
+}
