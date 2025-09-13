a210bbb61f3d9ad7835f9e7649cfb5795e9c2421
Manish0085 <manishk57107@gmail.com>
2025-09-14 04:27:46 +0530

    Adding Bom project for managing good amount of redundant work

---
+++ Section-20/eazy-bom/src/main/java/com/eazybytes/eazy_bom/EazyBomApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes.eazy_bom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyBomApplication.class, args);
+	}
+
+}
---
+++ Section-20/eazy-bom/src/test/java/com/eazybytes/eazy_bom/EazyBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.eazy_bom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyBomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
